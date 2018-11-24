58d84adbf316289278031e663cad6e05835e63e2
kobydamari <koby.damari@playbuzz.com>
2018-11-24 17:40:33 +0200

    debounce

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,6 +1,5 @@
 export const debounce = (fn, time) => {
     let timeout;
-    console.log(fn);
     debugger;
     return function () {
         const functionCall = () => fn.apply(this, arguments);
